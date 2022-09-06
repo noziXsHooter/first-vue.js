bcdc408a7d3555eb7a0aec8b61592da9504fe3a5
noziX <94942615+noziXsHooter@users.noreply.github.com>
2022-09-05 21:42:35 -0300

    add BaseAlert template

---
+++ app/src/components/BaseAlert.vue
@@ -0,0 +1,16 @@
+<template>
+    
+    <div class="alert">
+        Seu formulário foi enviado com sucesso!
+    </div>
+
+</template>
+
+
+<style>
+.alert {
+    padding: 5px;
+    border-radius: 6px;
+    color: gray;
+}
+</style>
